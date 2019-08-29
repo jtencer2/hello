b48f2ec8932cde3e33283068666d5146dae2ea84
jtencer2 <54672654+jtencer2@users.noreply.github.com>
2019-08-29 08:53:09 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+*Author: John Tencer and RJ Lierman
+*Date: 2019/8/29
+*
+*A simple hello world program in C
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("John Tencer and RJ Lierman!\n");
+
+  return 0;
+}
